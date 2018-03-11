ed226d9cbed52b5d557a719393c2dd8ffcb62bb3
Lee Kang Wenn <kangwenn@gmail.com>
2018-03-12 02:28:03 +0800

    Adds Upload IC function

--- app/src/main/java/com/example/kangwenn/currexez/SuccessPaymentActivity.java
+++ app/src/main/java/com/example/kangwenn/currexez/SuccessPaymentActivity.java
@@ -2,7 +2,6 @@ package com.example.kangwenn.currexez;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
